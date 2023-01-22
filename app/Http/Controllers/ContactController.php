a8c68d80f5fc00bd7f6672c4537348fd0651dc1a
Umayantha93 <umayantha93@gmail.com>
2023-01-22 12:05:48 +0530

    setup dropdown for the search button in main page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use App\Models\Company;
 
 class ContactController extends Controller
 {
     //
     public function index() {
 
+        $companies = Company::orderBy('name')->pluck('name','id');
         $contacts = Contact::orderBy('first_name', 'asc')->paginate(10);
 
-
-        return view('contacts.index', compact('contacts'));
+        return view('contacts.index', compact('contacts', 'companies'));
     }
 
     public function create() {
