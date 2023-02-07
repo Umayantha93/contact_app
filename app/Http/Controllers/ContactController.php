d88da5273be54a62fc5df8be185b8555cf6c7209
Umayantha93 <umayantha93@gmail.com>
2023-02-07 11:54:28 +0530

    setup frontend add

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -22,7 +22,19 @@ class ContactController extends Controller
     }
 
     public function create() {
-        return view('contacts.create');
+        $companies = Company::orderBy('name')->pluck('name','id')->prepend('All Companies');
+        return view('contacts.create', compact('companies'));
+    }
+
+
+    public function store(Request $request) {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'address' => 'required',
+            'company_id' => 'required|exists:companies,id',
+        ]);
     }
 
     public function show($id) {
